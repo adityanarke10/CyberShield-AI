a2b0daff1bcddf59ce158af3f9fd012893b8d236
Aditya Narke <adityanarke10@gmail.com>
2026-08-06 23:21:19 +0530

    changes

--- config.py
+++ config.py
@@ -2,23 +2,12 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+DB_FOLDER = os.path.join(BASE_DIR, "database")
 
-class Config:
+os.makedirs(DB_FOLDER, exist_ok=True)
 
-    SECRET_KEY = os.environ.get(
-        "SECRET_KEY",
-        "cybershield_ai_2026_secret_key"
-    )
+DB_PATH = os.path.join(DB_FOLDER, "cybershield.db")
 
-    database_path = os.path.join(
-        BASE_DIR,
-        "database",
-        "cybershield.db"
-    )
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "sqlite:///" + database_path
-    )
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + DB_PATH
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
