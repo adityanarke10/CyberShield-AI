8d9ef670e33a884e0b978ad8e66d52984eabfbbf
Aditya Narke <adityanarke10@gmail.com>
2026-08-06 22:56:54 +0530

    deployement trial-1

--- app.py
+++ app.py
@@ -43,10 +43,10 @@ def home():
     return render_template("index.html")
 
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
+if __name__ == "__main__":
     app.run(debug=True)
 
 
--- config.py
+++ config.py
@@ -2,9 +2,23 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-class Config:
-    SECRET_KEY = "cybershield_ai_2026_secret_key"
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database", "cybershield.db")
+class Config:
+
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "cybershield_ai_2026_secret_key"
+    )
+
+    database_path = os.path.join(
+        BASE_DIR,
+        "database",
+        "cybershield.db"
+    )
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///" + database_path
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
